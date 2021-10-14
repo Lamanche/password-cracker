cd8d9bee87930cdfd3dde511fe83b143c24e1768
Andres <andressiirak@hotmail.com>
2021-10-14 20:45:20 +0300

    done

--- script.js
+++ script.js
@@ -11,19 +11,20 @@ import { hashedPasswords } from "./hashedPasswords.js";
   const a = saltSet.length;
   for (let i = 0; i < a; i++) {
     //let possibleSalt = [];
-    let possibleSalt = saltSet[i];
+    //let possibleSalt = saltSet[i];
     //possibleSalt.push(saltSet[i]);
 
     const b = saltSet.length;
     for (let j = 0; j < b; j++) {
       //possibleSalt.push(saltSet[j]);
-      possibleSalt += saltSet[j];
+      //possibleSalt += saltSet[j];
 
       const c = hashedPasswords.length;
       for (let k = 0; k < c; k++) {
         const d = passwords.length;
         for (let l = 0; l < d; l++) {
-          const saltedPassword = possibleSalt.join("") + passwords[l];
+          //const saltedPassword = possibleSalt.join("") + passwords[l];
+          cons saltedPassword = saltSet[i]+saltSet[j]+password[l]
           const hashedPassword = CryptoJS.MD5(saltedPassword).toString();
           count++;
           if (hashedPassword === hashedPasswords[k]) {
@@ -36,7 +37,7 @@ import { hashedPasswords } from "./hashedPasswords.js";
         }
       }
       //possibleSalt.pop();
-      possibleSalt = saltSet[i];
+      //possibleSalt = saltSet[i];
     }
   }
   const endTime = new Date().getTime();
